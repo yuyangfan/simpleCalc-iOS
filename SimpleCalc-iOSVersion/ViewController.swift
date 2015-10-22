14b9435b9d0f0d21d8a721e1a8c93fe5be6805f0
Tony Fan <yf8@uw.edu>
2015-10-21 21:23:30 -0700

    Initial Commit

---
+++ SimpleCalc-iOSVersion/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimpleCalc-iOSVersion
+//
+//  Created by Yuyang Fan on 10/21/15.
+//  Copyright © 2015 Yuyang Fan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
